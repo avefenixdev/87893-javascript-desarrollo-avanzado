f6e9475ea4f0f0f2b28ee5935b59dd30fbb18890
Ave Fenix Dev <avefenixdev@gmail.com>
2026-07-29 10:10:51 -0300

    Agregamos backend para trabajar con binarios

--- clase-07/01.drag-and-drop-binarios/src/main.js
+++ clase-07/01.drag-and-drop-binarios/src/main.js
@@ -1,1 +1,6 @@
 import './style.css'
+
+const dropArea = document.getElementById('drop-area')
+const inputFile = document.querySelector('#input-file') // selectores css -> . para la clase | # para el id
+console.log(dropArea)
+console.log(inputFile)
