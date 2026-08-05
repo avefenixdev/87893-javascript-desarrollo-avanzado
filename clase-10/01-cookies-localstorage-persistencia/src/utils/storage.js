becd40335d3f37bf1aa2f43c0b3f29d595bc1718
Ave Fenix Dev <avefenixdev@gmail.com>
2026-08-05 10:19:49 -0300

    Creando nuestro utilitario para el storage

---
+++ clase-10/01-cookies-localstorage-persistencia/src/utils/storage.js
@@ -0,0 +1,61 @@
+
+
+//export const storage = {  // ! 1 manera
+const storage = {
+    set: (key, value) => {
+        window.localStorage.setItem(key, JSON.stringify(value))
+    },
+    get(key) {
+        const value = localStorage.getItem(key)
+
+        // Controlar si value es una cadena
+        if ( typeof value === 'string' ) {
+            return value
+        }
+
+        return JSON.parse(value)
+    },
+    getManejoError: (key) => {
+        
+        try {
+            const value = localStorage.getItem(key)
+            return JSON.parse(value)
+        } catch (error) {
+            console.error(error)
+            return value
+        }
+
+    },
+    remove: (key) => {
+        localStorage.removeItem(key)
+    },
+    clear: function() {
+        localStorage.clear()
+    }
+}
+
+//export const PI = 3.1415 // ! 1 manera
+const PI = 3.1415 // PI, no lo exporto por lo cual queda privado dentro de este arcchivo
+
+//export { storage } // todas las que quiera. // ! 2 manera (Hago público lo que quiero utilizar en otro archivo)
+
+export default storage // Funcionalidad principal, solo puedo tener una.
+
+//     ⬇️ func pri | ⬇️ ⬇️ func extra
+
+// ! Forma de incoporar (usar) el módulo en otros archivos
+//import storage, {PI} from 'react'
+
+// ! Ejemplo de como estaría exportado REACT
+/* 
+const React = {}
+
+//export const useState = () => {}
+const useState = () => {}
+// export const useEffect = () => {}
+const useEffect = () => {}
+
+export { useState, useEffect }
+export default React
+
+import React, {useState, useEffect} from 'react' */
